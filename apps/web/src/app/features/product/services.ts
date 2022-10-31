26a7e387986fddc6aad25d230cf55559738dabfc
xencodes <xencodes@icloud.com>
2022-10-31 09:05:34 +0530

    feat: add endpoints

---
+++ apps/web/src/app/features/product/services.ts
@@ -0,0 +1,22 @@
+import { Product } from '@prisma/client';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const productApi = createApi({
+  reducerPath: 'pokemonApi',
+  baseQuery: fetchBaseQuery(),
+  endpoints: (builder) => ({
+    // todo: sperate function for searching
+    getProducts: builder.query<Product[], string | void>({
+      query: (query) => `/api/products${query ? `?query=${query}` : ''}`,
+    }),
+    createProduct: builder.mutation<Product, Partial<Product>>({
+      query: (data) => ({
+        url: `/api/products`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useGetProductsQuery, useCreateProductMutation } = productApi;

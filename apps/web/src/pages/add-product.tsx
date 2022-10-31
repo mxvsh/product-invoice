886254de6063abb32ad932229fdd2ca3e3ac5b1b
xencodes <xencodes@icloud.com>
2022-10-31 09:04:53 +0530

    chore: clean up

--- apps/web/src/pages/add-product.tsx
+++ apps/web/src/pages/add-product.tsx
@@ -1,90 +1,10 @@
-import {
-  Box,
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  InputLeftAddon,
-  Stack,
-} from '@chakra-ui/react';
-import { Product } from '@prisma/client';
-import { MainLayout } from '@product-invoice/ui';
-import { useForm } from 'react-hook-form';
-import { FiCheck } from 'react-icons/fi';
-import Sidebar from '../components/Sidebar';
+import { MainLayout, Sidebar } from '@product-invoice/ui';
+import ProductEntry from '../app/features/product/ProductEntry';
 
 function AddProduct() {
-  const form = useForm<Partial<Product>>();
-
-  const handleSubmit = (data: Partial<Product>) => {
-    const payload: Partial<Product> = {
-      name: data.name,
-      price: Number(data.price),
-      quantity: Number(data.quantity),
-      category_id: 2,
-    };
-
-    fetch('/api/products', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  };
-
   return (
     <MainLayout title="Add Product" sidebar={<Sidebar />}>
-      <form onSubmit={form.handleSubmit(handleSubmit)}>
-        <Stack>
-          <FormControl>
-            <FormLabel fontSize="sm">Product Name</FormLabel>
-            <Input
-              size="sm"
-              placeholder="Enter product name"
-              {...form.register('name')}
-            />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel fontSize="sm">Product Price</FormLabel>
-            <InputGroup size="sm">
-              <InputLeftAddon>₹</InputLeftAddon>
-              <Input
-                type="number"
-                size="sm"
-                placeholder="Enter product price"
-                step="any"
-                {...form.register('price')}
-              />
-            </InputGroup>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel fontSize="sm">Product Quantity (optional)</FormLabel>
-            <Input
-              type="number"
-              size="sm"
-              placeholder="Enter product quantity"
-              {...form.register('quantity')}
-            />
-          </FormControl>
-
-          <Box>
-            <Button
-              colorScheme="purple"
-              size="sm"
-              leftIcon={<FiCheck />}
-              type="submit"
-            >
-              Submit
-            </Button>
-          </Box>
-        </Stack>
-      </form>
+      <ProductEntry />
     </MainLayout>
   );
 }
--- apps/web/src/pages/view-products.tsx
+++ apps/web/src/pages/view-products.tsx
@@ -1,8 +1,12 @@
-import { MainLayout } from '@product-invoice/ui';
-import Sidebar from '../components/Sidebar';
+import { MainLayout, Sidebar } from '@product-invoice/ui';
+import ProductList from '../app/features/product/ProductList';
 
 function ViewProducts() {
-  return <MainLayout title="View Products" sidebar={<Sidebar />}></MainLayout>;
+  return (
+    <MainLayout title="View Products" sidebar={<Sidebar />}>
+      <ProductList />
+    </MainLayout>
+  );
 }
 
 export default ViewProducts;

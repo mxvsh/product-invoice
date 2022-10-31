db132ba77a8e5122393a020c048357e4d19796d5
xencodes <xencodes@icloud.com>
2022-10-31 09:04:06 +0530

    chore: add findProducts

--- apps/web/src/pages/api/products.ts
+++ apps/web/src/pages/api/products.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { searchProducts, createProduct } from '@product-invoice/services';
+import {
+  searchProducts,
+  createProduct,
+  findProducts,
+} from '@product-invoice/services';
 
 export default async function products(
   req: NextApiRequest,
@@ -20,8 +24,12 @@ export default async function products(
   const query = req.query.query;
 
   if (query) {
+    // todo: cache value...
     const products = await searchProducts(query as string);
     res.status(200).json(products);
     return;
   }
+
+  const products = await findProducts();
+  res.status(200).json(products);
 }
--- libs/services/src/products.ts
+++ libs/services/src/products.ts
@@ -1,12 +1,19 @@
 import { Product } from '@prisma/client';
 import { dbClient } from './db';
 
+export const findProducts = () => {
+  return dbClient.product.findMany({
+    include: {
+      category: true,
+    },
+  });
+};
+
 export const searchProducts = (query: string) => {
   return dbClient.product.findMany({
     where: {
       name: {
         contains: query,
-        mode: 'insensitive',
       },
     },
   });
